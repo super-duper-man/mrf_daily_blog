b48319bb5c89fe2643329b0e9546e7ce27b374ff
unknown <m.r.fouladi7@gmail.com>
2023-12-17 16:34:20 +0330

    login initialized by github OAuth

--- components/nav/LoginForm.tsx
+++ components/nav/LoginForm.tsx
@@ -1,10 +1,32 @@
-import React from "react";
+"use client";
+import { createBrowserClient } from "@supabase/ssr";
 import { Button } from "../ui/button";
 import { SiGithub } from "react-icons/si";
+import { usePathname } from "next/navigation";
 
 export default function LoginForm() {
+  const pathName = usePathname();
+
+  const supabase = createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  );
+
+  const handleLogin = () => {
+    supabase.auth.signInWithOAuth({
+      provider: "github",
+      options: {
+        redirectTo: location.origin + `/auth/callback?next=${pathName}`,
+      },
+    });
+  };
+
   return (
-    <Button variant="outline" className="flex items-center gap-2">
+    <Button
+      variant="outline"
+      className="flex items-center gap-2"
+      onClick={handleLogin}
+    >
       <SiGithub />
       Login
     </Button>
